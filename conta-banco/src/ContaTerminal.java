cc645e567b309aad2ece828572a8d40c028b1013
victor hugo <victor.vetoriza@gmail.com>
2024-11-15 12:59:15 -0300

    meu projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,21 +1,35 @@
 import java.util.Scanner;
 
-import javax.print.attribute.standard.NumberOfDocuments;
-
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+
+        Scanner valores = new Scanner(System.in);
 
     int Numero;
     String NomeCliente, Agencia, Saldo;
     Double SaldoAnt;
-    
 
-    
-    
-        === Novo Banco do Povo ===    
-    Por favor Victor , digite o número da Agência!    
-    Quanto de saldo deseja depositar?
-    agora por favor digite sua conta:
-        
+
+    System.out.println(" === Novo Banco do Povo ===");
+    System.out.println("");
+
+    System.out.println("Por favor digite seu nome: ");
+    NomeCliente = valores.nextLine();
+    System.out.println("");
+
+    System.out.println("Por favor " +NomeCliente+ ", digite o número da Agência!");
+    Agencia = valores.nextLine();
+    System.out.println("");
+
+    System.out.println(NomeCliente+", agora por favor digite sua conta:");
+    Numero = valores.nextInt();
+    System.out.println("");
+
+    System.out.println("Quanto de saldo deseja depositar?");
+    SaldoAnt = valores.nextDouble();
+    Saldo = String.format("%.2f", SaldoAnt);
+
+    System.out.println("Olá "+NomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+Agencia+", conta "+Numero+" e seu saldo R$ "+Saldo+" já está disponível para saque.");
+ 
     }
 }
