47394ff1ed0b027339483a74d45928dcf6a69e2f
victor hugo <victor.vetoriza@gmail.com>
2024-11-15 12:40:09 -0300

    meu projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
+import javax.print.attribute.standard.NumberOfDocuments;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    int Numero;
+    String NomeCliente, Agencia, Saldo;
+    Double SaldoAnt;
+    
+
+    
+    
+        === Novo Banco do Povo ===    
+    Por favor Victor , digite o número da Agência!    
+    Quanto de saldo deseja depositar?
+    agora por favor digite sua conta:
+        
     }
 }
